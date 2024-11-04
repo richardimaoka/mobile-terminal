8441738dc7144a01a1bbf81708c0bd11c8d804b1
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-04 19:29:36 +0900

    beforeRun determined by index

--- nextjs/app/components/TerminalComponent.tsx
+++ nextjs/app/components/TerminalComponent.tsx
@@ -7,19 +7,28 @@ export default function TerminalComponent() {
       id: 1,
       type: "command",
       prompt: "myuser>",
-      command: "sleep 100000000000000",
+      // command: "sleep 100000000000000",
+      command: "echo abcde",
+      beforeRun: true,
+    },
+    {
+      id: 2,
+      type: "command",
+      prompt: "myuser>",
+      // command: "sleep 100000000000000",
+      command: "echo abcde",
       beforeRun: true,
     },
   ];
 
   return (
     <div className={styles.comoponent}>
-      {items.map((x) => (
+      {items.map((x, i) => (
         <Command
           key={x.id}
           prompt={x.prompt}
           command={x.command}
-          beforeRun={x.beforeRun}
+          beforeRun={items.length -1 === i}
         ></Command>
       ))}
     </div>
